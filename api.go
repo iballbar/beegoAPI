c33eb69538bcc848f3c7f6cb8402d4de0eecd585
iBallbar <bestballs@gmail.com>
2017-09-20 10:56:26 +0700

    added: ResponseJSONWithCode

--- api.go
+++ api.go
@@ -17,6 +17,12 @@ type ResponseObject struct {
 	ResponseObject interface{} `json:"responseObject"`
 }
 
+type ResponseObjectWithCode struct {
+	Code           int64       `json:"code"`
+	Message        string      `json:"message"`
+	ResponseObject interface{} `json:"responseObject"`
+}
+
 func (api *API) BaseURL() string {
 	var baseUrl string = api.Ctx.Input.Site() + fmt.Sprintf(":%d", api.Ctx.Input.Port())
 	if api.Ctx.Input.Header("X-Forwarded-Host") != "" {
@@ -26,6 +32,9 @@ func (api *API) BaseURL() string {
 }
 
 func (api *API) ResponseJSON(results interface{}, code int, msg string) {
+	if results == nil {
+		results = struct{}{}
+	}
 	response := &ResponseObject{
 		Code:           code,
 		Message:        msg,
@@ -33,6 +42,21 @@ func (api *API) ResponseJSON(results interface{}, code int, msg string) {
 	}
 	api.Data["json"] = response
 	api.Ctx.Output.SetStatus(code)
+	api.ServeJSON()
+	return
+}
+
+func (api *API) ResponseJSONWithCode(results interface{}, statusCode int, code int64, msg string) {
+	if results == nil {
+		results = struct{}{}
+	}
+	response := &ResponseObjectWithCode{
+		Code:           code,
+		Message:        msg,
+		ResponseObject: results,
+	}
+	api.Data["json"] = response
+	api.Ctx.Output.SetStatus(statusCode)
 	api.ServeJSON()
 	return
 }
