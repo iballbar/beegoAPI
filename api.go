c358892953ddb1d37ee34a0f5b35c66400f20007
iBallbar <bestballs@gmail.com>
2025-03-27 11:19:42 +0700

    refactor: test pr

--- api.go
+++ api.go
@@ -6,31 +6,35 @@ import (
 	beego "github.com/beego/beego/v2/adapter"
 )
 
-//API for Client
+// API for Client
 type API struct {
 	beego.Controller
 }
 
+// ResponseObject represents a standard API response with int code
 type ResponseObject struct {
 	Code           int         `json:"code"`
 	Message        string      `json:"message"`
 	ResponseObject interface{} `json:"responseObject"`
 }
 
+// ResponseObjectWithCode represents a standard API response with int64 code
 type ResponseObjectWithCode struct {
 	Code           int64       `json:"code"`
 	Message        string      `json:"message"`
 	ResponseObject interface{} `json:"responseObject"`
 }
 
+// BaseURL returns the base URL for the current request
 func (api *API) BaseURL() string {
-	var baseUrl string = api.Ctx.Input.Site() + fmt.Sprintf(":%d", api.Ctx.Input.Port())
+	baseUrl := api.Ctx.Input.Site() + fmt.Sprintf(":%d", api.Ctx.Input.Port())
 	if api.Ctx.Input.Header("X-Forwarded-Host") != "" {
 		baseUrl = api.Ctx.Input.Scheme() + "://" + api.Ctx.Input.Header("X-Forwarded-Host")
 	}
 	return baseUrl
 }
 
+// ResponseJSON sends a JSON response with the given results, code, and message
 func (api *API) ResponseJSON(results interface{}, code int, msg string) {
 	if results == nil {
 		results = struct{}{}
@@ -43,9 +47,10 @@ func (api *API) ResponseJSON(results interface{}, code int, msg string) {
 	api.Data["json"] = response
 	api.Ctx.Output.SetStatus(code)
 	api.ServeJSON()
-	return
 }
 
+// ResponseJSONWithCode sends a JSON response with the given results, HTTP status code, 
+// custom int64 code, and message
 func (api *API) ResponseJSONWithCode(results interface{}, statusCode int, code int64, msg string) {
 	if results == nil {
 		results = struct{}{}
@@ -58,5 +63,4 @@ func (api *API) ResponseJSONWithCode(results interface{}, statusCode int, code i
 	api.Data["json"] = response
 	api.Ctx.Output.SetStatus(statusCode)
 	api.ServeJSON()
-	return
 }
